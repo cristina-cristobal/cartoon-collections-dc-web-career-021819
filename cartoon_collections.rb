17470761ea58205c8b3b0ebc7d49d85c65d6c251
Cristina Cristobal <cristina.cristobal@gmail.com>
2019-02-07 19:05:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |name, index|
     index += 1
     puts "#{index}. #{name}"
-  end 
+  end
 end
 
 def summon_captain_planet(planeteer_calls)
